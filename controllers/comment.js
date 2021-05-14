46f8f58825c4a9a4ce43a434590bb2c045a171ab
rykesz <adfourretout@hotmail.fr>
2021-05-14 10:21:14 +0200

    backend fini

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,52 +1,57 @@
-//const { json } = require('sequelize/types');
-const User = require('../models/user');
-const Publication = require('../models/publication_seq');
-const Comment = require('../models/comment_seq');
+const sql = require('../models/db');
 
 exports.createComment = async (req, res) => {
-    // Récupère le texte du commentaire, ainsi que l'uuid de son auteur et de la publciation à laquelle il apparatient
-    const { text, autorUuid, publicationUuid } = req.body;
+    // Récupère le texte du commentaire, ainsi que l'userId de son auteur, et l'id de la publciation à laquelle il apparatient
+    const text = req.body.text;
+    const autorId = req.body.userId;
+    const pubId = req.body.pubId;
     try {
-        // Cherche l'auteur et la publication dans la BDD avec leur uuid
-        const user = await User.findOne({ where: { uuid: autorUuid } });
-        const publication = await Publication.findOne({ where: { uuid: publicationUuid } });
-        // Récupère la string-liste des ids des commentaires de la publication et la stocke sous forme d'array (en séparant les valeurs à chaque , )
-        const listComms = publication.comments.split(',');
+        const query = `INSERT INTO comments (text, autorId, pubOriginId) VALUES (?, ?, ?);`
+        const result = await sql.query(query, [text, autorId, pubId]);
+        const response = result[0];
+        console.log(response);
 
-        // Crée un nouveau commentaire et le sauvegarde dans la BDD
-        const comment = new Comment({ text, autorId: user.id});
-        await comment.save();
-        // Insère dans l'array-liste des ids des commentaires de la publication l'id du commentaire juste créé
-        await listComms.push(comment.id);
-        // Retransforme l'array-liste des ids de commentaires de la publication en string dans la publication (en séparant les valeurs par une , )
-        publication.comments = listComms.join(',');
+        // Fonctionnalité facultative ajoutant l'id du commentaire à commentsCreated de son auteur
+        /*const query2 = `SELECT commentsCreated FROM users WHERE id = ?`
+        const result2 = await sql.query(query2, autorId);
+        if (result2[0].affectedRows === 1) {
+            let commentsCreated = [];
+            if (response.commentsCreated != null) {
+                commentsCreated = response.commentsCreated.split(',');
+            }
+            commentsCreated.push(response.id);
+            const commentsCreatedToUpdate = commentsCreated.join(',');
+            const query2_2 = `UPDATE users SET commentsCreated = ? WHERE id = ?`;
+            result2_2 = await sql.query(query2_2, [commentsCreatedToUpdate, autorId]);
+        } else if (result2[0].affectedRows === 0) {
+            throw(error);
+        };*/
+        
 
-        // Sauvegarde la publication mise à jour
-        await publication.save();
-
-        return res.status(201).json({ message: 'Commentaire créé'});
-    } catch(err) {
-        console.log(err);
-        return res.status(500).json({ error: 'Could not create comment'});
+        if (result[0].affectedRows === 1) {
+            return res.status(201).json({ message: "comment created" });
+        } else if (result[0].affectedRows === 0) {
+            throw(error);
+        };
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json({ msg: 'Could not create comment' });
     };
 };
 
-// Envoie tous les commentaires d'une publication identifiée par son uuid
+// Envoie tous les commentaires d'une publication identifiée par son id
 exports.getAllComments = async (req, res) => {
-    const pubUuid = req.params.pubUuid;
+    const pubOriginId = req.params.pubId;
     try {
-        const publication = await Publication.findOne({ where: { uuid: pubUuid } });
-        const listComms = publication.comments.split(',');
-        const comments = [];
-        for (i = 0; i < listComms.length; i++) {
-            let newComment = await Comment.findOne({ where: { id: listComms[i]} });
-            comments.push(newComment);
-        }
-        if (comments.length === listComms.length) {
-            return res.status(201).json({comments});
-        } else {
-            return res.status(500).json({ message: 'Unable to load comments'})
-        }
+        const query = `SELECT * FROM comments WHERE pubOriginId = ? ORDER BY date_insertion ASC;`
+        const result = await sql.query(query, pubOriginId);
+        const response = result[0];
+        console.log(response);
+        if (response.length > 0) {
+            return res.status(200).json({ response });
+        } else if (response.length === 0) {
+            throw(error);
+        };
     } catch(err) {
         console.log(err);
         return res.status(500).json({ error: 'Could not find comment'});
@@ -54,9 +59,37 @@ exports.getAllComments = async (req, res) => {
 };
 
 exports.modifyComment = async (req, res) => {
-
+    const text = req.body.text;
+    const commId = req.params.commId;
+    console.log(commId);
+    try {
+        const query = `UPDATE comments SET text = ? WHERE id = ? ;`
+        const result = await sql.query(query, [text, commId]);
+        console.log(result[0]);
+        if (result[0].affectedRows === 1) {
+            return res.status(200).json({ message: "comment modified" });
+        } else if (result[0].affectedRows === 0) {
+            throw(error);
+        };
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Impossible de modifier le commentaire'});
+    };
 };
 
 exports.deleteComment = async (req, res) => {
+    const commId = req.params.commId;
+    try {
+        const query = "DELETE FROM comments WHERE id = ?;"
+        const result = await sql.query(query, commId);
 
+        if (result[0].affectedRows === 1) {
+            return res.status(200).json({ message: "comment deleted" });
+        } else if (result[0].affectedRows === 0) {
+            throw(error);
+        };
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json({ message: "Could not delete comment"});
+    };
 };
